1050f60805c7682040f2c46b1b15c5668fd59b2a
jennyoum <jennyoum@hotmail.com>
2019-09-08 23:57:51 -0400

    html line

---
+++ react-library/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello Dave and Jen!
+      Thank you for all that you have done for us!
+    </div>
+  );
+}
+
+export default App;
